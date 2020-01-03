1fb1d1265a98f04dd45a1c32753664e280139944
LOOR-ZAMBRANO-ROBERTH-DAVID <56706913+LOOR-ZAMBRANO-ROBERTH-DAVID@users.noreply.github.com>
2020-01-03 17:22:43 -0500

    Add files via upload
    Codigo sin los ConsoleWriteLine

---
+++ Clases/UserAdmin.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using EjemploDeMediador.Interfaces;
+using EjemploDeMediador.Clases;
+
+namespace EjemploDeMediador.Clases
+{
+    public class UserAdmin : IColleague
+    {
+        public UserAdmin(IMediator mediator) : base(mediator) /*Recibe el mediador*/
+        {
+        }
+        public override string Recive(string message)
+        {
+            return "Un administrador recibe: "+message;
+        }
+    }
+}
